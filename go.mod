f7ad1b04ab23b2bb8ea24cfdd94569a9fb6a789c
Tinashe Kudzanayi Mundangepfupfu <cylon85@gmail.com>
2023-11-25 00:03:28 +0100

    growth module
    Add dog growth module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tinashe-mundangepfupfu/puppy
 
 go 1.21.4
+
+require github.com/tinashe-mundangepfupfu/dog v0.0.0-20231124225846-393086ddd9d3 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import "github.com/tinashe-mundangepfupfu/dog"
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Bark())
 }
